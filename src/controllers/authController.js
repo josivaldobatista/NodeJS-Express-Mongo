beb3d9ba6d837eb79bb7a8b43aefe2f7dc699598
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-01-18 04:59:14 -0300

    Exception for email already exists and remove password for response.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,8 +5,16 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+  const { email } = req.body;
   try {
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: 'User already exists with this email.' })
+    }
+
     const user = await User.create(req.body);
+
+    user.password = undefined;
+
     return res.send({ user })
   } catch (err) {
     return res.status(400).send({ error: 'Registration failed' });
